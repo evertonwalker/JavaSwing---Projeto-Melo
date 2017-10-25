22f2bb7067cc0a649848347708791debe3b180c5
Flávio Macedo <fhenriquecm@hotmail.com>
2017-10-25 11:27:22 -0300

    Criação da classe OrdemServico

---
+++ src/estoque/modelos/OrdemServico.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estoque.modelos;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author FHC
+ */
+public class OrdemServico {
+    
+    private int numero;
+    private String descricao;
+    private Date date;
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
+    
+    
+    
+    
+}

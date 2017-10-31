ea0ea886932cd9fc11ec38539d133b841ced1e8e
fhenriquecm <fhenriquecm@hotmail.com>
2017-10-31 10:37:02 -0300

    Inclusao de novos atributos e métodos nas classes Mecanico, OrdemServiço e Pagamento

--- src/estoque/modelos/Pagamento.java
+++ src/estoque/modelos/Pagamento.java
@@ -16,6 +16,62 @@ public class Pagamento {
     private float valorPagamento;
     
     private OrdemServico ordemServico;
+
+    /**
+     * @return the numeroPagamento
+     */
+    public String getNumeroPagamento() {
+        return numeroPagamento;
+    }
+
+    /**
+     * @param numeroPagamento the numeroPagamento to set
+     */
+    public void setNumeroPagamento(String numeroPagamento) {
+        this.numeroPagamento = numeroPagamento;
+    }
+
+    /**
+     * @return the descricaoPagamento
+     */
+    public String getDescricaoPagamento() {
+        return descricaoPagamento;
+    }
+
+    /**
+     * @param descricaoPagamento the descricaoPagamento to set
+     */
+    public void setDescricaoPagamento(String descricaoPagamento) {
+        this.descricaoPagamento = descricaoPagamento;
+    }
+
+    /**
+     * @return the valorPagamento
+     */
+    public float getValorPagamento() {
+        return valorPagamento;
+    }
+
+    /**
+     * @param valorPagamento the valorPagamento to set
+     */
+    public void setValorPagamento(float valorPagamento) {
+        this.valorPagamento = valorPagamento;
+    }
+
+    /**
+     * @return the ordemServico
+     */
+    public OrdemServico getOrdemServico() {
+        return ordemServico;
+    }
+
+    /**
+     * @param ordemServico the ordemServico to set
+     */
+    public void setOrdemServico(OrdemServico ordemServico) {
+        this.ordemServico = ordemServico;
+    }
     
 }
 

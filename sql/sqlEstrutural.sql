0c6ba168d9d69ede1419dc7b1b97aa1437cbff63
Everton Walker <everton.walker@hotmail.com>
2017-10-31 11:04:47 -0300

    Criando sql estrutural

---
+++ sql/sqlEstrutural.sql
@@ -0,0 +1,27 @@
+CREATE TABLE CLIENTE_FISICO (
+ 
+
+cpf VARCHAR(14) PRIMARY KEY,
+
+nome VARCHAR(150) NOT NULL,
+
+email varchar(60)not null,
+
+telefonePrinc varchar(30) not null,
+
+telefoneOpc varchar(30),
+
+cep VARCHAR(9) not null,
+
+logradouro VARCHAR(90) not null,
+
+estado varchar(40) not null,	
+
+cidade varchar(40) not null,
+
+bairro varchar(40) not null,
+
+numero varchar(20) not null
+
+)
+

c231f0ca7eb3acf53c9eabc989bd7b7da8f1af65
fhenriquecm <fhenriquecm@hotmail.com>
2017-10-31 11:29:38 -0300

    Implementação da DAO ClientePessoaJuridica

--- sql/sqlEstrutural.sql
+++ sql/sqlEstrutural.sql
@@ -25,3 +25,38 @@ numero varchar(20) not null
 
 )
 
+CREATE DATABASE controle;
+
+USE controle;
+
+CREATE TABLE CLIENTE_JURIDICO (
+ 
+
+cnpj VARCHAR(14) PRIMARY KEY,
+
+nomeFantasia VARCHAR(150) NOT NULL,
+
+razaoSocial VARCHAR(150) NOT NULL,
+
+email varchar(60)not null,
+
+telefonePrinc varchar(30) not null,
+
+telefoneOpc varchar(30),
+
+cep VARCHAR(9) not null,
+
+logradouro VARCHAR(90) not null,
+
+estado varchar(40) not null,	
+
+cidade varchar(40) not null,
+
+bairro varchar(40) not null,
+
+numero varchar(20) not null
+
+)
+
+
+SELECT * FROM CLIENTE_JURIDICO

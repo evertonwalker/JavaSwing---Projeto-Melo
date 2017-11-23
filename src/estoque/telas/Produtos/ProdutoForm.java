74041ffad45bc6bee0af47f56a4022cc68dc5cd9
Everton Walker <everton.walker@hotmail.com>
2017-11-22 22:09:30 -0200

    modificação rápida

--- src/estoque/telas/Produtos/ProdutoForm.java
+++ src/estoque/telas/Produtos/ProdutoForm.java
@@ -72,7 +72,7 @@ public class ProdutoForm extends javax.swing.JFrame {
         valorDeVenda = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         jLabel1.setText("Comprar Produtos");

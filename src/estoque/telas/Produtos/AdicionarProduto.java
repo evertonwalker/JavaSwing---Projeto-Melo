5e511eeaf58c6f21a2fd38e5ac76b5f8e895c496
Everton Walker <everton.walker@hotmail.com>
2017-11-24 19:14:13 -0200

    Adicionar Produto criação

--- src/estoque/telas/Produtos/AdicionarProduto.java
+++ src/estoque/telas/Produtos/AdicionarProduto.java
@@ -5,6 +5,15 @@
  */
 package estoque.telas.Produtos;
 
+import estoque.controladores.ControladorMecanico;
+import estoque.controladores.ControladorProduto;
+import estoque.modelos.Mecanico;
+import estoque.modelos.Produto;
+import estoque.telas.ordemServicoTelas.OrdemServico;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author EWalker
@@ -12,12 +21,42 @@ package estoque.telas.Produtos;
 public class AdicionarProduto extends javax.swing.JFrame {
 
     /**
-     * Creates new form AdicionarProduto
+     * Creates new form ClienteListagem
      */
+    ArrayList<Produto> listaProdutoGlobal;
+    OrdemServico formPai;
+
     public AdicionarProduto() {
+
         initComponents();
+        this.setLocationRelativeTo(null);
     }
-    
+
+    public AdicionarProduto(OrdemServico formPai) {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.formPai = formPai;
+        listarProdutos();
+    }
+
+    public void listarProdutos() {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //atribuindo as colunas da tabela
+        modelo.setColumnIdentifiers(new String[]{"Descrição", "Referência" ,
+            "Aplicação", "Quantidade no Estoque" });
+        try {
+            ControladorProduto cp = new ControladorProduto();
+            this.listaProdutoGlobal = cp.listar();
+            for (Produto p : listaProdutoGlobal) {
+                modelo.addRow(new String[]{"" + p.getDescricao(), p.getReferencia(),
+                p.getAplicacao(), "" +p.getEstoqueAtual()});
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(rootPane, ex.getMessage());
+        }
+        tableProduto.setModel(modelo);
+
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -28,21 +67,144 @@ public class AdicionarProduto extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tableProduto = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        filtroProduto = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        voltarButton = new javax.swing.JButton();
+        confirmarClienteButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        tableProduto.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tableProduto);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Adicionar Produto");
+
+        filtroProduto.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                filtroProdutoKeyPressed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel2.setText("Filte pela Referência");
+
+        voltarButton.setText("Voltar");
+        voltarButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                voltarButtonActionPerformed(evt);
+            }
+        });
+
+        confirmarClienteButton.setText("Confirmar Produto");
+        confirmarClienteButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                confirmarClienteButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(156, 156, 156))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(filtroProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 386, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 183, Short.MAX_VALUE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(voltarButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(confirmarClienteButton)))
+                .addGap(27, 27, 27))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(filtroProduto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(voltarButton)
+                    .addComponent(confirmarClienteButton))
+                .addGap(30, 30, 30))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void filtroProdutoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_filtroProdutoKeyPressed
+        // TODO add your handling code here:
+
+        DefaultTableModel modelo = new DefaultTableModel();
+        //atribuindo as colunas da tabela
+        modelo.setColumnIdentifiers(new String[]{"Nome", "CPF"});
+
+        if (filtroProduto.getText().length() > 0) {
+            for (Produto p : listaProdutoGlobal) {
+                if (p.getReferencia().toLowerCase().contains(
+                        filtroProduto.getText().toLowerCase())) {
+                   modelo.addRow(new String[]{"" + p.getDescricao(), p.getReferencia(),
+                p.getAplicacao(), "" +p.getEstoqueAtual()});
+                }
+            }
+            tableProduto.setModel(modelo);
+        } else {
+            listarProdutos();
+        }
+    }//GEN-LAST:event_filtroProdutoKeyPressed
+
+    private void voltarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_voltarButtonActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_voltarButtonActionPerformed
+
+    private void confirmarClienteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_confirmarClienteButtonActionPerformed
+        // TODO add your handling code here:
+        int index = tableProduto.getSelectedRow();
+
+        if (index >= 0) {
+            Produto p = this.listaProdutoGlobal.get(index);
+            //formPai.receberProduto(p);
+            this.dispose();
+            formPai.setVisible(true);
+
+        } else {
+            JOptionPane.showMessageDialog(rootPane, "Selecione um Produto para"
+                    + " confirmar");
+        }
+
+    }//GEN-LAST:event_confirmarClienteButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -58,17 +220,56 @@ public class AdicionarProduto extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
@@ -80,5 +281,12 @@ public class AdicionarProduto extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton confirmarClienteButton;
+    private javax.swing.JTextField filtroProduto;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tableProduto;
+    private javax.swing.JButton voltarButton;
     // End of variables declaration//GEN-END:variables
 }
